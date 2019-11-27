5032ba1d171e933cb05cb252340253e21f11b875
RuknuddinAsrari <ruknuddin.asrari@gmail.com>
2019-11-27 18:07:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/proctorialsystem/data/LoginDataSource.java
@@ -0,0 +1,30 @@
+package com.example.proctorialsystem.data;
+
+import com.example.proctorialsystem.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TO DO: Handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+    //Used to logout of the app 
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
+ 
